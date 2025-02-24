64bc06bf6e33ade77bdf4e612647f0d2368eb318
maxikyo <holovanovmax@gmail.com>
2025-02-24 17:08:45 +0100

    fix

--- src/test/java/homework/homework2/TimestampExample.java
+++ src/test/java/homework/homework2/TimestampExample.java
@@ -9,11 +9,14 @@ public class TimestampExample {
         Timestamp t = new Timestamp(System.currentTimeMillis());
         Calendar cal = Calendar.getInstance();
         Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
         String currentDate = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy").format(t).toUpperCase();
         cal.add(Calendar.YEAR, -10);
-        cal1.add(Calendar.YEAR, +100);
+        cal1.add(Calendar.MONTH, -5);
+        cal2.add(Calendar.YEAR, +100);
         System.out.println(currentDate);
         System.out.println(cal.getTime());
         System.out.println(cal1.getTime());
+        System.out.println(cal2.getTime());
     }
 }
