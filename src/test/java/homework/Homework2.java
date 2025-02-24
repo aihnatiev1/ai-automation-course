fae2875e5f722f7f303c66e7439813beee80ce97
DKruh <dimakrug1803@gmail.com>
2025-02-24 17:47:48 +0200

    HW2kruh

---
+++ src/test/java/homework/Homework2.java
@@ -0,0 +1,44 @@
+package homework;
+
+import org.testng.annotations.Test;
+
+import java.sql.Timestamp;
+
+@Test
+//Неявне приведення
+public class Homework2 {
+    public static void main() {
+//        int i = 3;
+//        double d = i;
+//        System.out.println("Task 1: " + d);
+    }
+    @Test
+    public void di () {
+        int i = 3;
+        double d = i;
+        System.out.println("Test 1: " + d);
+    }
+
+    public void doubleToInt() {
+        double q = 3.14;
+        int t = (int) q;
+        System.out.println("Test 2: " + t);
+    }
+
+    public void operation1() {
+        int g = 3;
+        double c = 1.5;
+        double result = g * c;
+        System.out.println("Test 3: " + result);
+    }
+
+    public void operation2() {
+        int g = 3;
+        double c = 1.5;
+        int result = (int) (g / c);
+        System.out.println("Test 4: " + result);
+    }
+
+}
+
+
---
+++ src/test/java/homework/Homework2Time.java
@@ -0,0 +1,58 @@
+package homework;
+
+import org.testng.annotations.Test;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Homework2Time {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public static void currentTime() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH);
+        String fomatedTime = format.format(timestamp);
+        System.out.println("Current Timestamp: " + fomatedTime);
+    }
+
+    @Test
+    public static void tenYearsAgo() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp.getTime());
+        calendar.add(Calendar.YEAR, -10);
+        Timestamp someTimeAgo = new Timestamp(calendar.getTimeInMillis());
+        System.out.println("Ten year ago " + someTimeAgo);
+    }
+
+    @Test
+    public static void fiveMonthsAgo() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp.getTime());
+        calendar.add(Calendar.MONTH, -5);
+        Timestamp someTimeAgo = new Timestamp(calendar.getTimeInMillis());
+        System.out.println("Five months ago " + someTimeAgo);
+    }
+
+    @Test
+    public static void tenOYearAgo() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp.getTime());
+        calendar.add(Calendar.YEAR, -100);
+        Timestamp someTimeAgo = new Timestamp(calendar.getTimeInMillis());
+        System.out.println("One hundred year ago " + someTimeAgo);
+    }
+
+}
+
+
