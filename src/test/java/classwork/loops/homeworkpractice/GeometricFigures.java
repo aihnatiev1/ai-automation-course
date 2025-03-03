6a769fbb868019269d97b9a3d0a994f7ddfb6e04
maxikyo <holovanovmax@gmail.com>
2025-03-03 17:02:05 +0100

    DZ loops

--- src/test/java/classwork/loops/homeworkpractice/GeometricFigures.java
+++ src/test/java/classwork/loops/homeworkpractice/GeometricFigures.java
@@ -5,60 +5,71 @@ public class GeometricFigures {
 
         int star = 5;
 
-//        for (int i = 0; i <= star; i++) {
-//            for (int j = 0; j <= i; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//
-//        for (int i = 0; i <= star; i++) {
-//            for (int j = i; j <= star; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
+        System.out.println();
         for (int i = 0; i <= star; i++) {
-            for (int j = star; j > i; j--) {
-                System.out.println(" ");
-            }
-            for (int j1 = 0; j1 <= i; j1++) {
-                System.out.println("*");
-            }
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int i = 0; i <= star; i++) {
+            for (int j = i; j <= star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int i = 0; i <= star; i++) {
             for (int j = star; j > i; j--) {
                 System.out.print(" ");
             }
+
+            for (int j1 = 0; j1 <= i; j1++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
-//        int rectangle = 5;
+        System.out.println();
 
-//        for (int i = 0; i <= rectangle; i++) {
-//
-//            //Левое пустое пространство
-////            for (int j = rectangle; j > i; j--) {
-////                System.out.print(" ");
-////            }
-//
-//            //Левая часть треугольника
-//            for (int j1 = 0; j1 <= i; j1++) { //Здесь можно изменить счётчик или поставить равенство с внешним счётчиком что бы пирамидка была остроугольная
-//                System.out.print("+");
-//            }
-//
-//            //Правая часть треугольника
-//            for (int j = 0; j < i; j++) {
-//                System.out.print("+");
-//            }
-//
-//            //Правое пустое пространство
-//            for (int j = rectangle; j > i; j--) {
-//                System.out.print(" ");
-//            }
-//
-//            System.out.println("*" + i);
-//        }
+        for (int i = 0; i <= star; i++) {
+            for (int j = star; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j1 = 0; j1 <= i; j1++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = star -1; i >= 0; i--) {
+
+            for (int j = star; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j1 = 0; j1 <= i; j1++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
+
+
